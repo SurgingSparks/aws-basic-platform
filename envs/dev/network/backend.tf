34f555730f66e3c5ec7e25c6cd94d681aebe0b24
joclopez <joclopez@sice.com.au>
2025-10-17 23:24:34 +1100

    adding some terraform

---
+++ envs/dev/network/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "tf-state-testing-2025"
+    key            = "envs/dev/network/terraform.tfstate"
+    region         = "ap-southeast-2"
+    dynamodb_table = "tf-state-locks"
+    encrypt        = true
+  }
+}
